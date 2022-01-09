a2d959c634c8d5502ab66e3aeacefa42c4b3a8a0
prastabdkl <prastabdkl@gmail.com>
2022-01-09 11:00:17 -0600

    implement cnn prediction left

--- classify.py
+++ classify.py
@@ -8,17 +8,19 @@ from keras import backend as K
 # dimensions of our images.
 img_width, img_height = 512, 512
 
-train_data_dir = 'dataset/data_c_vs_nc/train'
-validation_data_dir = 'dataset/data_c_vs_nc/validate'
+train_data_dir = r'C:\Users\prast\Downloads\CS 7000\Final report\u-net-code\unet-for-covid-detection\dataset\data_c_vs_nc\train'
+validation_data_dir = r'C:\Users\prast\Downloads\CS 7000\Final report\u-net-code\unet-for-covid-detection\dataset\data_c_vs_nc\validate'
 nb_train_samples = 100
 nb_validation_samples = 10
 epochs = 20
 batch_size = 1
 
+print('modal initialization with input shape')
 if K.image_data_format() == 'channels_first':
     input_shape = (1, img_width, img_height)
 else:
     input_shape = (img_width, img_height, 1)
+print('modal initialization start')
 
 model = Sequential()
 model.add(Conv2D(32, (3, 3), input_shape=input_shape))
@@ -40,9 +42,13 @@ model.add(Dropout(0.5))
 model.add(Dense(1))
 model.add(Activation('sigmoid'))
 
+print('model initialization end')
+
 model.compile(loss='binary_crossentropy',
               optimizer='rmsprop',
               metrics=['accuracy'])
+
+print('compilation done')
 
 # this is the augmentation configuration we will use for training
 train_datagen = ImageDataGenerator(
@@ -50,10 +56,13 @@ train_datagen = ImageDataGenerator(
     shear_range=0.2,
     zoom_range=0.2,
     horizontal_flip=True)
+print('augmentation done')
 
 # this is the augmentation configuration we will use for testing:
 # only rescaling
 test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+print('train generator done')
 
 train_generator = train_datagen.flow_from_directory(
     train_data_dir,
@@ -61,11 +70,15 @@ train_generator = train_datagen.flow_from_directory(
     batch_size=batch_size,
     class_mode='binary')
 
+print('validation generatr done')
+
 validation_generator = test_datagen.flow_from_directory(
     validation_data_dir,
     target_size=(img_width, img_height),
     batch_size=batch_size,
     class_mode='binary')
+
+print('fitting the model')
 
 model.fit_generator(
     train_generator,
@@ -73,5 +86,7 @@ model.fit_generator(
     epochs=epochs,
     validation_data=validation_generator,
     validation_steps=nb_validation_samples // batch_size)
+
+print('saving the weights')
 
 model.save_weights('first_try.h5')
